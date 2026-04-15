33fdff7d8c08ad78e31359e0347921d296e7a3f1
cheolsu-9900 <csay9900@gmail.com>
2026-04-15 14:51:44 +0900

    Add files via upload

---
+++ schedule.data.js
@@ -0,0 +1,33 @@
+const START_YEAR = 2026;
+
+const SCHEDULE_DATA = [
+    {
+        month: 7,
+        regular:  { status: "upcoming", total: 2,  booked: 1 },
+    },
+
+    {
+        month: 8,
+        regular:  { status: "upcoming", total: 2,  booked: 1 },
+    },
+
+    {
+        month: 9,
+              regular:  { status: "upcoming", total: 2 },
+    },
+
+    {
+        month: 10,
+       regular:  { status: "upcoming", total: 2 },
+    },
+
+    {
+        month: 11,
+     regular:  { status: "upcoming", total: 2 },
+    },
+
+    {
+        month: 12,
+          regular:  { status: "upcoming", total: 2 },
+    },
+];
